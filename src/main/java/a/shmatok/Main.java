842d593c4c3de32a1b82532cd16f77d2d7fa2ff7
Shmatok Anton <ashmatok@gmail.com>
2023-10-24 21:50:31 +0300

    init commit

---
+++ src/main/java/a/shmatok/Main.java
@@ -0,0 +1,10 @@
+package a.shmatok;
+
+/**
+ * Created by anton on 24.10.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
