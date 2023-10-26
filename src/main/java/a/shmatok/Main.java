69f83cea51bf5c3f6984bd5d7c6677279c7630d8
Shmatok Anton <ashmatok@gmail.com>
2023-10-26 14:13:04 +0300

    new branch

--- src/main/java/a/shmatok/Main.java
+++ src/main/java/a/shmatok/Main.java
@@ -5,6 +5,6 @@ package a.shmatok;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 123123123");
+        System.out.println("Hello one more HELLO");
     }
 }
